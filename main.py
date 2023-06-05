d843ce0d9cd55be427f7d0a2c4a578e1a63f4608
JamToast13 <134562428+JamToast13@users.noreply.github.com>
2023-06-05 05:07:36 +0000

    we doubled the questions answers, added a fail system and changed the wording of the intro

--- main.py
+++ main.py
@@ -7,28 +7,30 @@ answers = globals
 
 
 # Questions and answers
-base_questions = ["Name a popular fruit.", "Name a popular country.", "Name a popular superhero.", "Name a popular movie."]
-base_answers = [
+questions = ["Name a popular fruit.", "Name a popular country.", "Name a popular superhero.", "Name a popular movie."]
+answers = [
     ["apple", "banana", "orange", "strawberry", "blue berry", "grape", "watermelon", "avocado"],
     ["usa", "canada", "japan", "australia", "italy", "china", "britain", "ukraine"],
     ["superman", "spider-man", "batman", "wonder woman", "iron man", "hulk", "thor", "the flash"],
     ["titanic", "star wars", "the avengers", "jurassic park", "toy story", "home alone", "die hard", "matrix"]
 ]
-questions = base_questions
-answers = base_answers
+
 
 def populate_list():
   pass
 
 def how_to_play():
-    print('\nHow to play:\nYou an another player will both be given a question')
+    print('\nHow to play:\nYou an another player will both be given a question\ntThen if you get it righ you will move on to the next round!\nBut, if you fail you will get a strike, 3 strikes and you lose! ')
 
 def play_game():
 	# Select a random question
 	question_index = random.randint(0, len(questions) - 1)
 	question = questions[question_index]
 	possible_answers = answers[question_index]
-	
+
+	#strikes
+	player1_strikes = 0
+	print(f'Player1: {player1_strikes} strikes')
 	print("Question:", question)
 	
 	# Shuffle the answers
@@ -41,10 +43,11 @@ def play_game():
 	# Check if the answer is correct
 	if user_choice.lower() in possible_answers:
 		print ('yes')
-		#answers.remove(user_choice)
+		answers.remove(user_choice)
 	else:
 		print ('no')
-        
+		player1_strikes +=1
+    
 # Main loop
 print('Welcome to Family Game Fight')
 know = input("Do you know how to play? (Y/N) ")
@@ -56,8 +59,8 @@ if know.lower() == 'n':
     
 while True:
     play_game()
-    play_again = input("Do you want to play again? (yes/no): ")
-    if play_again.lower() != "yes":
+    play_again = input("Do you want to play again? (y/n): ")
+    if play_again.lower() != "y":
         break
 
 print("Thank you for playing!")
